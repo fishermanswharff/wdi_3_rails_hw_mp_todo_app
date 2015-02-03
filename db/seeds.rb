06b5502a933313184a0385cee9b425775b514655
fishermanswharff <fishermanswharff@github.com>
2015-02-02 22:31:02 -0500

    seeds for tasks, show view on a list

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,13 @@
 List.destroy_all
+Task.destroy_all
 
 lists = List.create([{title: "Groceries", complete: 0},{title: "Work", complete: 0},{title: "Home", complete: 0}])
+lists[0].tasks << Task.create({label: "broccoli"})
+lists[0].tasks << Task.create({label: "eggs"})
+lists[0].tasks << Task.create({label: "bacon"})
+lists[0].tasks << Task.create({label: "bread"})
+lists[0].tasks << Task.create({label: "pasta"})
+lists[0].tasks << Task.create({label: "milk"})
+lists[0].tasks << Task.create({label: "orange juice"})
+lists[0].tasks << Task.create({label: "steaks"})
+lists[0].tasks << Task.create({label: "potatoes"})
